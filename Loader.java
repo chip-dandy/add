e75e1feb2b160a62fb60ba0414a422616a5c802c
Chip Dandy <chip.dandy@yandex.ru>
2019-06-25 00:13:16 +0300

    my first commit

---
+++ Loader.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Loader {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        ToDoList list = new ToDoList();
+        String text;
+
+        for(;;){
+            System.out.println("Введите команду...");
+            text = scanner.nextLine();
+
+            //System.out.println(text.substring(0, text.lastIndexOf(" ")).toUpperCase());
+            //String[] textDo = (text.split(" "));
+            //String[] t9 = text.split(" ");
+
+            if(text.length() > 4){
+//                if(text.substring(0,3).toUpperCase().equals("ADD")){
+//
+//                    list.addToList(text.substring(4));
+//                }else
+
+                    if(text.toUpperCase().matches("ADD [^0-9]+")){
+                    list.addToList(text.substring(4));
+
+                }else if(text.toUpperCase().matches("(ADD|EDIT) \\d+ a-zA-Z+? A-Z+?")){
+                    String t2 = text.substring(0,4).toUpperCase();
+                    //System.out.println(t2);
+                    if(t2.equals("ADD ")){
+                        String[] textArray = (text.replaceAll("\\D+", " ").trim()).split(" ");
+                        int[] a = new int[textArray.length];
+                        for (int i = 0; i < textArray.length; i++) {
+                            a[i] = Integer.valueOf(textArray[i]);
+                        }
+                        System.out.println(a[0]);
+                        list.addToListIndex(a[0], text.substring(4));
+                    }
+                    if (t2.equals("EDIT")){
+                        System.out.println("edit");
+                    }
+                }else if(text.toUpperCase().matches("(DELL) \\d+")){
+                    System.out.println("DELL");
+                }
+
+                //else if (text.substring(0,3).toUpperCase().equals("ADD") && )
+
+
+            }else if(text.length() == 4){
+                if(text.toUpperCase().equals("LIST")){
+                    System.out.println("Команда LIST!");
+                    list.getToDoList();
+                }
+            }
+
+
+
+        }
+    }
+}
